acbfebbcfb21364f4a93d365e69d84fe908fa0df
AlessadnroADP <alessandro.davila@tecsup.edu.pe>
2025-04-26 12:17:25 -0500

    vocal

--- app/src/test/java/com/davila/alessandro/laboratoriocalificado01/Ejercicio01Kotlin.kt
+++ app/src/test/java/com/davila/alessandro/laboratoriocalificado01/Ejercicio01Kotlin.kt
@@ -25,22 +25,4 @@ class Ejercicio01Kotlin {
         println(result)
         return result
     }
-
-    @Test
-    fun vocal() {
-        val texto = "Hola Mundo"
-        val vocales = contarV(texto.lowercase())
-        println(vocales)
-    }
-    @Test
-    fun vocal2() {
-        val texto = "Kotlin es genial"
-        val vocales = contarV(texto.lowercase())
-        println(vocales)
-    }
-
-    private fun contarV(frase: String): Int {
-        return frase.count { it in "aeiou" }
-    }
-
 }
--- app/src/test/java/com/davila/alessandro/laboratoriocalificado01/Ejercicio02Kotlin.kt
+++ app/src/test/java/com/davila/alessandro/laboratoriocalificado01/Ejercicio02Kotlin.kt
@@ -1,4 +1,22 @@
 package com.davila.alessandro.laboratoriocalificado01
+import org.junit.Test
 
 class Ejercicio02Kotlin {
+
+    @Test
+    fun vocal() {
+        val texto = "Hola Mundo"
+        val vocales = contarV(texto.lowercase())
+        println(vocales)
+    }
+    @Test
+    fun vocal2() {
+        val texto = "Kotlin es genial"
+        val vocales = contarV(texto.lowercase())
+        println(vocales)
+    }
+
+    private fun contarV(frase: String): Int {
+        return frase.count { it in "aeiou" }
+    }
 }
